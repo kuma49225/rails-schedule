4245d0a586210afd6ed9a9e3e64dd489ebfc65a0
熊谷翔太 <syota@kumagaiutanoAir.lan>
2024-05-25 17:08:28 +0900

    初回提出

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,5 +1,16 @@
 class Plan < ApplicationRecord
-  validates :title, presence: true
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-end
+    validates :title, presence: true
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+    validate :end_date_after_start_date
+  
+    private
+  
+    def end_date_after_start_date
+      return if end_date.blank? || start_date.blank?
+  
+      if end_date < start_date
+        errors.add(:end_date, "must be after the start date")
+      end
+    end
+  end
